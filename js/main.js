ac858594559251586c6e4d41b4bd5da74e192c93
Mehgan Iulo <holler@mehgan.com>
2014-02-17 16:00:10 -0500

    added Owl Carousel responsive image slider plug-in

--- js/main.js
+++ js/main.js
@@ -3,10 +3,25 @@
     $("#vimeo-iframe").fitVids();
 
     //Responsive image carousel
-    $("#owl-example").owlCarousel();
+    $("#image-slider").owlCarousel({
+    	items: 1, 
+      itemsDesktop: false,
+      itemsDesktopSmall: false,
+      itemsTablet: false,
+      itemsMobile: false,
+      autoHeight: true,
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      // autoPlay: 5000
+		navigation: true,
+		navigationText: [
+		"<i class='fa fa-angle-left'></i>",
+		"<i class='fa fa-angle-right'></i>"
+		]
 
 
 
+    });
 
 
 
