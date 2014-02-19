f91b365eb5da64cce0113253c6e622ad31549622
Mehgan Iulo <holler@mehgan.com>
2014-02-19 16:27:00 -0500

    added projects 1 & 2 image assets. animation for HMDA hp

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@
     //--------Display header on scroll---------
     function showHeader() {
       if ($(window).scrollTop() > 500) {
-        $("header").show();
-        // alert("hi");
-      } 
+        $("header").slideDown(100);
+      }
       else {
         $("header").hide();
       }
@@ -13,6 +12,41 @@
     $(window).scroll(showHeader); // check on scroll
     showHeader();// check on load
 
+    //--------Auto scroll HMDA homepage---------
+    function animateImages() {
+      $(".browser_container img").each(function(){
+        var frameHeight = $(".browser_container").height();
+        var newTop = ($(this).height() - frameHeight) * -1;
+        
+        if( parseInt($(this).css( "top" ), 10) >= -1) {
+          $(this).animate({
+            top: newTop
+          }, 10000, "swing", function() {
+            $(this).stop(true, false);
+            $(this).css( "top", newTop );
+            animateImages();
+          });
+        }
+        // if( parseInt($(this).css( "top" ), 10) <= newTop + 1) {
+        //   $(this).animate({
+        //     top: 0
+        //   }, 7000, "swing", function() {
+        //     $(this).stop(true, false);
+        //     $(this).css( "top", 0 );
+        //     animateImages();
+        //   });
+        // }
+      });
+    }
+    animateImages();
+
+    $(".browser_container img").each(function(){
+      $(this).stop(true, false);
+      $(this).css( "top", 0 );
+      setTimeout(function (){ 
+        animateImages();
+      }, 2000);
+    });
 
     //--------Fluid-width video player---------
     $("#vimeo-iframe").fitVids();
