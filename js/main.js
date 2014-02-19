cdee9534bea74ced152bec593f7c061ab0d9e10b
Mehgan Iulo <holler@mehgan.com>
2014-02-18 19:35:21 -0500

    display header nav only when user scrolls beneath #intro

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
   $(document).ready(function(){
-    // Fluid-width video player
+
+    //--------Display header on scroll---------
+    function showHeader() {
+      if ($(window).scrollTop() > 500) {
+        $("header").show();
+        // alert("hi");
+      } 
+      else {
+        $("header").hide();
+      }
+    }
+    $(window).scroll(showHeader); // check on scroll
+    showHeader();// check on load
+
+
+    //--------Fluid-width video player---------
     $("#vimeo-iframe").fitVids();
 
-    //Responsive image carousel
+    //--------Responsive image carousel--------
     $("#image-slider").owlCarousel({
     	items: 1, 
       itemsDesktop: false,
@@ -18,17 +33,13 @@
 		// "<i class='fa fa-angle-left'></i>",
 		// "<i class='fa fa-angle-right'></i>"
 		// ]
-
-
     });
-
-   $(".fa-angle-right").click(function(){
-		$("#image-slider").trigger('owl.next');
-	});
-
-	$(".fa-angle-left").click(function(){
-		$("#image-slider").trigger('owl.prev');
-	});
+    $(".fa-angle-right").click(function(){
+  		$("#image-slider").trigger('owl.next');
+  	});
+  	$(".fa-angle-left").click(function(){
+  		$("#image-slider").trigger('owl.prev');
+  	});
 
 
 
