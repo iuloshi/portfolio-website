154dd9f552f07448fc4c2132d08f52d2920fcb21
Mehgan Iulo <holler@mehgan.com>
2014-05-13 20:24:25 -0400

    fixed HMDA site animation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
 
-//------------------Call fullPage JS------------------
+//------------------Call fullPage.js------------------
 
-  $.fn.fullpage();
-
-//--------------Display header on scroll--------------
-
-  // $(window).scroll(function(){
-  //   $("header").slideDown(250);
-  // });
+  $.fn.fullpage({
+    afterSlideLoad: function(anchorLink, index){
+      if( index==2 ) {
+        animateImage();
+      }
+    },
+    onSlideLeave: function(anchorLink, index){
+      $(".browser_container img").stop(true, false);
+      resetImage();
+    }
+  });
 
 //--------Browser animation for HMDA homepage---------
-  function animateImages() {
+  function animateImage() {
     $(".browser_container img").each(function(){
       var frameHeight = $(".browser_container").height();
       var newTop = ($(this).height() - frameHeight) * -1;
@@ -22,37 +26,17 @@ $(document).ready(function(){
         }, 10000, "swing", function() {
           $(this).stop(true, false);
           $(this).css( "top", newTop );
-          animateImages();
+          animateImage();
         });
       }
-      // if( parseInt($(this).css( "top" ), 10) <= newTop + 1) {
-      //   $(this).animate({
-      //     top: 0
-      //   }, 7000, "swing", function() {
-      //     $(this).stop(true, false);
-      //     $(this).css( "top", 0 );
-      //     animateImages();
-      //   });
-      // }
     });
   }
-  //Fire animation when user scrolls beyond Intro section
-  function initBrowserScroll() {
-    if ($(window).scrollTop() > 500) {
-      animateImages();
-    }
-  }
-  $(window).scroll(initBrowserScroll); 
-  initBrowserScroll();
 
-  // ***NOT WORKING:*** Restart animation when resize window
-  $(window).resize(function(){
-    animateImages();
-  });
-  $(window).resize(animateImages); 
+  function resetImage() {
+    $(".browser_container img").delay( 4000 ).css( "top", 0 );
+  }
   
 //--------FitVids fluid-width video player---------
   $("#vimeo-iframe").fitVids();
-
 
 });
