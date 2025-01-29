e0b4f603f535c7b339c727296359fe041a7090c4
Mehgan Iulo <mehganiulo@Arabella.local>
2025-01-29 11:33:44 -0500

    trying something

--- 2025/eleventy.config.js
+++
@@ -1,9 +0,0 @@
-export default function(eleventyConfig) {
-	// Input directory: src 
-	// Output directory: _site
-	// Copies css & images from input to output directory
-
-	eleventyConfig.addPassthroughCopy({ "src/css": "css" });
-	eleventyConfig.addWatchTarget("./src/css/"); // hot reloads local server after CSS updates
-	eleventyConfig.addPassthroughCopy({ "src/img": "img" });
-  };
